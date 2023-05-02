de840e4093126a475689b833c5f09e6895df625a
Aliyu <sadiqful@gmail.com>
2023-05-02 23:00:25 +0100

    authroute added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = 3000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
@@ -11,6 +13,11 @@ mongoose.connect(process.env.MONGO_URL)
     .catch((err) => {
         console.log(err)
     });
+
+app.use(express.json())
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is starting on PORT ${PORT}`)
