76700e1e8acf1fff7ebab66d29b3c884b6ab5e42
Aliyu <sadiqful@gmail.com>
2023-05-09 16:54:25 +0100

    login and register endpoint works fine

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,88 @@
 const router = require("express").Router();
-const CryptoJs = require("crypto-js");
+const CryptoJS = require("crypto-js");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt")
+const Joi = require("joi")
+
+// VALIDATION 
+
+const registerSchema = Joi.object({
+    username: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+  
+  const loginSchema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
 
 //REGISTRATION
 
 router.post("/register", async (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
+    const { error } = registerSchema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    //Check if the user is allready in the db
+    const emailExists = await User.findOne({ email: req.body.email });
+  
+    if (emailExists) return res.status(400).send("Email already taken");
+  
+    //hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+  
+    //create new user
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashPassword,
     });
+  
+    try {
+      const savedUser = await user.save();
+      res.send(savedUser);
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  });
 
+  // LOGIN
+
+router.post("/login", async (req, res) => {
     try {
 
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser)
-        
-    } catch (error) {
-        res.status(500).json(error)
+    const { error } = loginSchema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    const user = await User.findOne({ email: req.body.email });
+  
+    if (!user) return res.status(400).send("Email or password is wrong");
+  
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send("Email or password is wrong");
+  
+    //Create and assign a token
+
+    const accessToken = jwt.sign(
+        {
+            id: user._id,
+            isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SEC,
+            {expiresIn:"3d"}
+        );
+  
+        const { password, ...others } = user._doc;  
+        res.status(200).json({...others, accessToken});
+
+
+    } catch(err) {
+        res.status(500).json(err);
     }
-});
+  });
+
+
 
 module.exports = router;
