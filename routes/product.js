b6377d45514a711eff536db9aa42707559255775
Aliyu <sadiqful@gmail.com>
2023-05-12 07:07:23 +0100

    delete product endpoint

--- routes/product.js
+++ routes/product.js
@@ -35,4 +35,18 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
     }
 });
 
+// DELETE PRODUCT
+
+router.delete("/:id", verifyAndAuthorization, async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json("Product has been deleted...");
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+
 module.exports = router
