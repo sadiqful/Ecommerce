092772de8382841bccea08f37f6af927885f4411
Aliyu <sadiqful@gmail.com>
2023-05-12 06:09:26 +0100

    get user endpoint

--- routes/user.js
+++ routes/user.js
@@ -41,7 +41,19 @@ router.delete("/:id", verifyAndAuthorization, async (req, res) => {
     }
 });
 
-// GET USERS 
+// GET USER 
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+    
+    const user = await User.findById(req.params.id);
+    const { password, ...others} = user._doc;
+    res.status(200).json(others);
+    
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 
 
