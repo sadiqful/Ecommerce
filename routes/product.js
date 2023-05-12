f0f9b389beb6828f5df78cbeda4b2ee7488f6155
Aliyu <sadiqful@gmail.com>
2023-05-12 14:48:57 +0100

    get all product endpoint

--- routes/product.js
+++ routes/product.js
@@ -60,4 +60,29 @@ router.get("/find/:id", async (req, res) => {
     }
 });
 
+// GET ALL PRODUCT 
+
+router.get("/", async (req, res) => {
+    const qNew = req.query.new 
+    const qCategory = req.query.category
+
+    try {
+
+        let products 
+
+        if(qNew) {
+            products = await Product.find().sort({ $createdAt: - 1}).limit(1);
+
+        } else if (qCategory) {
+            products = await Product.find({ categories: { $in: [qCategory], } });
+
+        } else {
+            products = await Product.find();
+        }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 module.exports = router
