e937acab031c396b61f6ce3fdf331483bb966f23
Aliyu <sadiqful@gmail.com>
2023-05-12 06:51:31 +0100

    product route added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user")
-const authRoute = require("./routes/auth")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product")
+const cors = require("cors");
 
 dotenv.config();
 
@@ -14,10 +16,11 @@ mongoose.connect(process.env.MONGO_URL)
         console.log(err)
     });
 
-app.use(express.json())
-
+app.use(express.json());
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/product', productRoute);
+app.use(cors());
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is starting on PORT ${PORT}`)
