de189834a635123578eb03678e543dc2e0e3f969
Aliyu <sadiqful@gmail.com>
2023-05-12 07:03:46 +0100

    update product endpoint

--- routes/product.js
+++ routes/product.js
@@ -9,11 +9,27 @@ const {
 // CREATE PRODUCT
 
 router.post("/", verifyTokenAndAdmin, async (req, res) => {
-    const createdProduct = new Product(req.body);
+    const newProduct = new Product(req.body);
 
     try {
-        const savedProduct = await createdProduct.save();
+        const savedProduct = await newProduct.save();
         res.status(200).json(savedProduct);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// UPDATE PRODUCT
+
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+
+    try {
+    
+        const updatedProduct = Product.findByIdAndUpdate(
+        req.params.id, {$set: req.body}, {new: true }    
+    );
+    
+        res.status(200).json(updatedProduct);
     } catch (error) {
         res.status(500).json(error);
     }
