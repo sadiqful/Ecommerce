8ae620e0544ef83cdfba78e51676bf5885fd84a6
Aliyu <sadiqful@gmail.com>
2023-05-12 06:18:10 +0100

    get all users endpoint

--- routes/user.js
+++ routes/user.js
@@ -34,10 +34,10 @@ router.put("/:id", verifyAndAuthorization, async (req, res) => {
 router.delete("/:id", verifyAndAuthorization, async (req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has been deleted...")
+        res.status(200).json("User has been deleted...");
 
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).json(error);
     }
 });
 
@@ -50,6 +50,20 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
     const { password, ...others} = user._doc;
     res.status(200).json(others);
     
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// GET ALL USERS 
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new;
+
+    try {
+    const users = query ? await User.find().sort({ _id: -1 }).limit(5) : await User.find();
+    res.status(200).json(users);
+
     } catch (error) {
         res.status(500).json(error);
     }
