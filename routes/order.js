24cad73951028bb58dc3db29c6a1fcd47bd16882
Aliyu <sadiqful@gmail.com>
2023-05-13 11:41:13 +0100

    create order

--- routes/order.js
+++ routes/order.js
@@ -1,10 +1,24 @@
 const router = require("express").Router();
 const {
     verifyToken,
-    verifyAndAuthorization,
+    verifyTokenAndAuthorization,
     verifyTokenAndAdmin
 } = require("./verifyToken")
 const Order = require("../models/Order")
+
+// Allow users with verified token to create order 
+
+router.post("/", verifyToken, async (req, res) => {
+    const newOrder = new Order(req.body);
+
+    try {
+        const savedOrder = await newOrder.save()
+        res.status(200).json(savedOrder);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 
 
 module.exports = router
