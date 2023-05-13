12a469057af0d40d8a31c1365019072afb167130
Aliyu <sadiqful@gmail.com>
2023-05-13 21:05:31 +0100

    get monthly income

--- routes/order.js
+++ routes/order.js
@@ -66,6 +66,23 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
     }
 });
 
+// get monthly income
 
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth - 1));
+    const previousMonth = new Date(date.setMonth(lastMonth.getMonth - 1));
+
+    try {
+        const income = await Order.aggregate([
+            {$match: {createdAt: {$gte: previousMonth } } },
+            {$project: {$month: "createdAt" }, sales: '$amount' },
+            {$group: {$_id: "month", $total: {$sum: "$sales" }, }, },
+        ]);
+        res.status(200).json(income);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router
