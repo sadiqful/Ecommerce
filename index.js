d146cccee03e6069561ca3c945fe5b08f3514a7f
Aliyu <sadiqful@gmail.com>
2023-05-13 20:04:24 +0100

    added order and cart route

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
-const productRoute = require("./routes/product")
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 const cors = require("cors");
 
 dotenv.config();
@@ -20,6 +22,8 @@ app.use(express.json());
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/product', productRoute);
+app.use('/api/cart', cartRoute);
+app.use('/api/order', orderRoute);
 app.use(cors());
 
 app.listen(process.env.PORT || PORT, () => {
